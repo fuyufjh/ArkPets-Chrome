0a7a8c5679238f8b7065ce6d9917212438a4ed97
Eric Fu <fuyufjh@gmail.com>
2025-01-14 00:43:50 +0800

    Change domain match rule to suffix match

--- src/lib/utils.test.ts
+++ src/lib/utils.test.ts
@@ -2,33 +2,19 @@ import { describe, it, expect } from 'vitest'
 import { compareSemver, matchDomain } from './utils'
 
 describe('match function', () => {
-  it('matches exact domains', () => {
+  it('returns true if domain matches pattern exactly', () => {
     expect(matchDomain('www.google.com', 'www.google.com')).toBe(true)
   })
 
-  it('matches wildcard subdomains', () => {
-    expect(matchDomain('www.google.com', '*.google.com')).toBe(true)
+  it('returns true if domain ends with pattern', () => {
+    expect(matchDomain('www.google.com', 'google.com')).toBe(true)
+    expect(matchDomain('www.google.com', 'com')).toBe(true)
   })
 
-  it('does not match when subdomain is missing', () => {
-    expect(matchDomain('www.google.com', 'google.com')).toBe(false)
-  })
-
-  it('matches multiple wildcards', () => {
-    expect(matchDomain('www.google.com', '*.google.*')).toBe(true)
-    expect(matchDomain('www.google.com', 'www.*.com')).toBe(true)
-  })
-
-  it('does not match different TLDs', () => {
-    expect(matchDomain('www.google.com', 'google.us')).toBe(false)
-  })
-
-  it('matches wildcard TLD', () => {
-    expect(matchDomain('www.google.com', '*.com')).toBe(true)
-  })
-
-  it('matches universal wildcard', () => {
-    expect(matchDomain('www.google.com', '*')).toBe(true)
+  it('returns false if domain does not match pattern', () => {
+    expect(matchDomain('www.google.com', 'ogle.com')).toBe(false)
+    expect(matchDomain('www.google.com', 'www.google')).toBe(false)
+    expect(matchDomain('google.com', 'www.google.com')).toBe(false)
   })
 }) 
 
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,17 +5,9 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-// Match domain names with wildcard (`*`)
+// Test if domain matches pattern.
 export function matchDomain(domain: string, pattern: string): boolean {
-  // Convert patterns to regex-safe strings and replace * with regex pattern
-  const escapeRegex = (str: string) => str.replaceAll(/[.*+?^${}()|[\]\\]/g, '\\$&').replaceAll('\\*', '.*')
-  
-  // Create regex pattern from the wildcard pattern
-  const regexPattern = `^${escapeRegex(pattern)}$`
-  
-  // Create RegExp object and test the domain
-  const regex = new RegExp(regexPattern)
-  return regex.test(domain)
+  return domain === pattern || domain.endsWith('.' + pattern);
 }
 
 // Compare semver version. Return true if v1 is greater than v2.
